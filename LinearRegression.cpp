4bedc1c7e99fbe20d94102bfe107eebe4fb19b52
haianhle176 <ducduc14706@gmail.com>
2026-05-09 22:43:34 +0700

    VIF update

--- LinearRegression.cpp
+++ LinearRegression.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 #include "MLLIB.h"
 #include<fstream>	
-#define lr 0.2
-#define epoch 10000
+#include <chrono>
+#define lr 0.5
+#define epoch 400
 using namespace std;
-const int N = 1000, D = 20, K = 10;
+using namespace std::chrono;
+const int N = 500, D = 10, K = 1; // Sample - Feature - Output
+
 int main(){
 	Dataset S(N,D,K);
-	Weight P(D,K);
+	Weight P(D,K); P.initial(0,0);
 	Loss_History L;
 	string sel, fs_sel;
 	float t;
 	ifstream fin("dataset.txt");
 	cin >> sel >> fs_sel;;
 	for (int i = 0;i < S.N;i++){
-		for (int j = 0;j<S.D;j++){
-			fin >> t;
-			S.atX(i,j) = t;
-		}
-		for (int j = 0;j<S.K;j++){
-			fin >> t;
-			S.atY(i,j) = t;
-		}
+		for (int j = 0;j<S.D;j++){fin >> S.atX(i,j);}
+		for (int j = 0;j<S.K;j++){fin >> S.atY(i,j);}
 	}
     fin.close();
-    P.initial(0,0);
+    vector<float> VIF_arr(D);
+	VIF_Cal(S,VIF_arr.data());
+	cout << "VIF: ";
+	for (float x : VIF_arr){
+		cout << x << " ";
+	}
+    auto start = high_resolution_clock::now();
     Scaler scaler(D);
-    feature_scaling(S,fs_sel, scaler);
+  	feature_scaling(S,fs_sel, scaler);
 	LinearRegression(S, P, lr, epoch, sel, L);
 	rescale_weights(P, scaler);
-	L.show();
+	//L.show();
+	auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "\n\n--- KET QUA ---" << endl;
+    cout << "Thoi gian thuc thi: " << duration.count() << " ms" << endl;
 	P.show();
 }
